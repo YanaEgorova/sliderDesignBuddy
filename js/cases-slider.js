1b9fedf570ecd410cd5ecd535dfe019c4105bdf5
YanaEgorova <45537884+YanaEgorova@users.noreply.github.com>
2020-09-29 00:17:07 +0300

    Update cases-slider.js

--- js/cases-slider.js
+++ js/cases-slider.js
@@ -1,9 +1,8 @@
 var swiper1 = new Swiper('.swiper-container.cases_swiper-container', {
-  loop: true,
+ 
   breakpoints: {
     320: {
       slidesPerView: 4,
-      spaceBetween: 30,
     },
     768: {
       slidesPerView: 6,
